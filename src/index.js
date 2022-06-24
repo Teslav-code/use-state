450c13a08946794000c47eac87454f68d4a20b93
Teslav-code <teslav93@mail.ru>
2022-06-24 12:48:17 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState }from 'react';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const App = () => {
+  return ( 
+    <div>
+      <HookSwitcher />
+    </div>
+  );
+}
+
+ const HookSwitcher = () => {
+
+  const [ color, setColor ] = useState('gray');
+  const [ fontSize, setFontSize ] = useState(14);
+
+  return (
+    <div style ={{ padding: '10px', 
+      backgroundColor: color,
+      fontSize: `${fontSize }px`}}>
+        Hello World
+        <button 
+        onClick ={() => setColor('gray')}>
+          Dark
+        </button>
+        <button onClick ={() => setColor('white')}>
+          Light
+        </button>
+        <button
+        onClick={() => setFontSize((s) => s + 2)}>
+          +
+        </button>
+    </div>
+  ); 
+ };
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>
+ );
+ 
+
+
+
+
